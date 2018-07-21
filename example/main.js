7347a77c45d7005cef5329f90f8a76e161837e8f
Josh Hargreaves <joshyhargreaves@gmail.com>
2018-07-21 12:47:47 -0400

    Add prettier format command & format files (#12)
    * Add pre-commit hooks with lint-staged
    
    * Update files
    
    * Add prettierignore dotfile

--- example/main.js
+++ example/main.js
@@ -1,40 +1,110 @@
-import React from 'react'
-import { Dimensions, View } from 'react-native'
+import React from 'react';
+import { Dimensions, View } from 'react-native';
 
-import EventCalendar from 'react-native-events-calendar'
+import EventCalendar from 'react-native-events-calendar';
 
-let { width } = Dimensions.get('window')
+let { width } = Dimensions.get('window');
 
 export default class App extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       events: [
-        { start: '2017-09-06 22:30:00', end: '2017-09-06 23:30:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
-        { start: '2017-09-07 00:30:00', end: '2017-09-07 01:30:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
-        { start: '2017-09-07 01:30:00', end: '2017-09-07 02:20:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
-        { start: '2017-09-07 04:10:00', end: '2017-09-07 04:40:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
-        { start: '2017-09-07 01:05:00', end: '2017-09-07 01:45:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
-        { start: '2017-09-07 14:30:00', end: '2017-09-07 16:30:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
-        { start: '2017-09-08 01:20:00', end: '2017-09-08 02:20:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
-        { start: '2017-09-08 04:10:00', end: '2017-09-08 04:40:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
-        { start: '2017-09-08 00:45:00', end: '2017-09-08 01:45:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
-        { start: '2017-09-08 11:30:00', end: '2017-09-08 12:30:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
-        { start: '2017-09-09 01:30:00', end: '2017-09-09 02:00:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
-        { start: '2017-09-09 03:10:00', end: '2017-09-09 03:40:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
-        { start: '2017-09-09 00:10:00', end: '2017-09-09 01:45:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
-        { start: '2017-09-10 12:10:00', end: '2017-09-10 13:45:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' }
-      ]
-    }
+        {
+          start: '2017-09-06 22:30:00',
+          end: '2017-09-06 23:30:00',
+          title: 'Dr. Mariana Joseph',
+          summary: '3412 Piedmont Rd NE, GA 3032',
+        },
+        {
+          start: '2017-09-07 00:30:00',
+          end: '2017-09-07 01:30:00',
+          title: 'Dr. Mariana Joseph',
+          summary: '3412 Piedmont Rd NE, GA 3032',
+        },
+        {
+          start: '2017-09-07 01:30:00',
+          end: '2017-09-07 02:20:00',
+          title: 'Dr. Mariana Joseph',
+          summary: '3412 Piedmont Rd NE, GA 3032',
+        },
+        {
+          start: '2017-09-07 04:10:00',
+          end: '2017-09-07 04:40:00',
+          title: 'Dr. Mariana Joseph',
+          summary: '3412 Piedmont Rd NE, GA 3032',
+        },
+        {
+          start: '2017-09-07 01:05:00',
+          end: '2017-09-07 01:45:00',
+          title: 'Dr. Mariana Joseph',
+          summary: '3412 Piedmont Rd NE, GA 3032',
+        },
+        {
+          start: '2017-09-07 14:30:00',
+          end: '2017-09-07 16:30:00',
+          title: 'Dr. Mariana Joseph',
+          summary: '3412 Piedmont Rd NE, GA 3032',
+        },
+        {
+          start: '2017-09-08 01:20:00',
+          end: '2017-09-08 02:20:00',
+          title: 'Dr. Mariana Joseph',
+          summary: '3412 Piedmont Rd NE, GA 3032',
+        },
+        {
+          start: '2017-09-08 04:10:00',
+          end: '2017-09-08 04:40:00',
+          title: 'Dr. Mariana Joseph',
+          summary: '3412 Piedmont Rd NE, GA 3032',
+        },
+        {
+          start: '2017-09-08 00:45:00',
+          end: '2017-09-08 01:45:00',
+          title: 'Dr. Mariana Joseph',
+          summary: '3412 Piedmont Rd NE, GA 3032',
+        },
+        {
+          start: '2017-09-08 11:30:00',
+          end: '2017-09-08 12:30:00',
+          title: 'Dr. Mariana Joseph',
+          summary: '3412 Piedmont Rd NE, GA 3032',
+        },
+        {
+          start: '2017-09-09 01:30:00',
+          end: '2017-09-09 02:00:00',
+          title: 'Dr. Mariana Joseph',
+          summary: '3412 Piedmont Rd NE, GA 3032',
+        },
+        {
+          start: '2017-09-09 03:10:00',
+          end: '2017-09-09 03:40:00',
+          title: 'Dr. Mariana Joseph',
+          summary: '3412 Piedmont Rd NE, GA 3032',
+        },
+        {
+          start: '2017-09-09 00:10:00',
+          end: '2017-09-09 01:45:00',
+          title: 'Dr. Mariana Joseph',
+          summary: '3412 Piedmont Rd NE, GA 3032',
+        },
+        {
+          start: '2017-09-10 12:10:00',
+          end: '2017-09-10 13:45:00',
+          title: 'Dr. Mariana Joseph',
+          summary: '3412 Piedmont Rd NE, GA 3032',
+        },
+      ],
+    };
   }
 
-  _eventTapped (event) {
-    alert(JSON.stringify(event))
+  _eventTapped(event) {
+    alert(JSON.stringify(event));
   }
 
-  render () {
+  render() {
     return (
-      <View style={{flex: 1, marginTop: 20}}>
+      <View style={{ flex: 1, marginTop: 20 }}>
         <EventCalendar
           eventTapped={this._eventTapped.bind(this)}
           events={this.state.events}
@@ -44,6 +114,6 @@ export default class App extends React.Component {
           scrollToFirst
         />
       </View>
-    )
+    );
   }
 }
--- src/DayView.js
+++ src/DayView.js
@@ -1,193 +1,213 @@
 // @flow
-import {
-    View,
-    Text,
-    ScrollView,
-    TouchableOpacity
-} from 'react-native'
-import populateEvents from './Packer'
-import React from 'react'
-import moment from 'moment'
-import _ from 'lodash'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
+import populateEvents from './Packer';
+import React from 'react';
+import moment from 'moment';
+import _ from 'lodash';
 
-const LEFT_MARGIN = 60 - 1
+const LEFT_MARGIN = 60 - 1;
 // const RIGHT_MARGIN = 10
-const CALENDER_HEIGHT = 2400
+const CALENDER_HEIGHT = 2400;
 // const EVENT_TITLE_HEIGHT = 15
-const TEXT_LINE_HEIGHT = 17
+const TEXT_LINE_HEIGHT = 17;
 // const MIN_EVENT_TITLE_WIDTH = 20
 // const EVENT_PADDING_LEFT = 4
 
-function range (from, to) {
-    return Array.from(Array(to), (_, i) => from + i)
+function range(from, to) {
+  return Array.from(Array(to), (_, i) => from + i);
 }
 
 export default class DayView extends React.PureComponent {
-    constructor (props) {
-        super(props)
-        this.calendarHeight = (props.end - props.start) * 100
-        const width = props.width - LEFT_MARGIN
-        const packedEvents = populateEvents(props.events, width, props.start)
-        let initPosition = _.min(_.map(packedEvents, 'top')) - this.calendarHeight / (props.end - props.start)
-        initPosition = initPosition < 0 ? 0 : initPosition
-        this.state = {
-            _scrollY: initPosition,
-            packedEvents
-        }
-
-    }
-
-    componentWillReceiveProps (nextProps) {
-        const width = nextProps.width - LEFT_MARGIN
-        this.setState({
-            packedEvents: populateEvents(nextProps.events, width, nextProps.start)
-        })
-    }
-
-    componentDidMount () {
-        this.props.scrollToFirst && this.scrollToFirst()
-    }
-
-    scrollToFirst () {
-        setTimeout(() => {
-            if (this.state && this.state._scrollY && this._scrollView) {
-                this._scrollView.scrollTo({ x: 0, y: this.state._scrollY, animated: true })
-            }
-        }, 1)
-    }
-
-    _renderRedLine() {
-        const offset = 100
-        const { format24h } = this.props
-        const { width, styles } = this.props
-        const timeNowHour = moment().hour()
-        const timeNowMin = moment().minutes()
-        return (
-            <View key={`timeNow`}
-                  style={[styles.lineNow, { top: offset * (timeNowHour - this.props.start) + offset * timeNowMin / 60, width: width - 20 }]}
-            />
-        )
-    }
-
-    _renderLines () {
-        const { format24h, start, end } = this.props
-        const offset = this.calendarHeight / (end - start)
-
-        return range(start, end + 1).map((i, index) => {
-            let timeText
-            if (i === start) {
-                timeText = ``
-            } else if (i < 12) {
-                timeText = !format24h ? `${i} AM` : i
-            } else if (i === 12) {
-                timeText = !format24h ? `${i} PM` : i
-            } else if (i === 24) {
-                timeText = !format24h ? `12 AM` : 0
-            } else {
-                timeText = !format24h ? `${i - 12} PM` : i
-            }
-            const { width, styles } = this.props
-            return [
-                <Text
-                    key={`timeLabel${i}`}
-                    style={[styles.timeLabel, { top: offset * index - 6 }]}
-                >
-                    {timeText}
-                </Text>,
-                i === start ? null : (
-                    <View
-                        key={`line${i}`}
-                        style={[styles.line, { top: offset * index, width: width - 20 }]}
-                    />
-                ),
-                <View
-                    key={`lineHalf${i}`}
-                    style={[styles.line, { top: offset * (index + 0.5), width: width - 20 }]}
-                />
-            ]
-        })
+  constructor(props) {
+    super(props);
+    this.calendarHeight = (props.end - props.start) * 100;
+    const width = props.width - LEFT_MARGIN;
+    const packedEvents = populateEvents(props.events, width, props.start);
+    let initPosition =
+      _.min(_.map(packedEvents, 'top')) -
+      this.calendarHeight / (props.end - props.start);
+    initPosition = initPosition < 0 ? 0 : initPosition;
+    this.state = {
+      _scrollY: initPosition,
+      packedEvents,
     };
+  }
 
-    _renderTimeLabels () {
-        const { styles, start, end } = this.props
-        const offset = this.calendarHeight / (end - start)
-        return range(start, end).map((item, i) => {
-            return (
-                <View key={`line${i}`} style={[styles.line, { top: offset * i }]} />
-            )
-        })
-    }
+  componentWillReceiveProps(nextProps) {
+    const width = nextProps.width - LEFT_MARGIN;
+    this.setState({
+      packedEvents: populateEvents(nextProps.events, width, nextProps.start),
+    });
+  }
 
-    _onEventTapped (event) {
-        this.props.eventTapped(event)
-    };
+  componentDidMount() {
+    this.props.scrollToFirst && this.scrollToFirst();
+  }
 
-    _renderEvents () {
-        const { styles } = this.props
-        const { packedEvents } = this.state
-        let events = packedEvents.map((event, i) => {
-            const style = {
-                left: event.left,
-                height: event.height,
-                width: event.width,
-                top: event.top
-            };
+  scrollToFirst() {
+    setTimeout(() => {
+      if (this.state && this.state._scrollY && this._scrollView) {
+        this._scrollView.scrollTo({
+          x: 0,
+          y: this.state._scrollY,
+          animated: true,
+        });
+      }
+    }, 1);
+  }
 
-            const eventColor = {
-                backgroundColor: event.color,
-            };
+  _renderRedLine() {
+    const offset = 100;
+    const { format24h } = this.props;
+    const { width, styles } = this.props;
+    const timeNowHour = moment().hour();
+    const timeNowMin = moment().minutes();
+    return (
+      <View
+        key={`timeNow`}
+        style={[
+          styles.lineNow,
+          {
+            top:
+              offset * (timeNowHour - this.props.start) +
+              (offset * timeNowMin) / 60,
+            width: width - 20,
+          },
+        ]}
+      />
+    );
+  }
 
-            // Fixing the number of lines for the event title makes this calculation easier.
-            // However it would make sense to overflow the title to a new line if needed
-            const numberOfLines = Math.floor(event.height / TEXT_LINE_HEIGHT)
-            const formatTime = this.props.format24h ? 'HH:mm' : 'hh:mm A'
-            return (
-                <View
-                    key={i}
-                    style={[styles.event, style, event.color && eventColor]}
-                >
-                    {this.props.renderEvent ? this.props.renderEvent(event) : (
-                        <TouchableOpacity
-                            activeOpacity={0.5}
-                            onPress={() => this._onEventTapped(this.props.events[event.index])}
-                        >
-                            <Text numberOfLines={1} style={styles.eventTitle}>{event.title || 'Event'}</Text>
-                            {numberOfLines > 1
-                                ? <Text
-                                    numberOfLines={numberOfLines - 1}
-                                    style={[styles.eventSummary]}
-                                >
-                                    {event.summary || ' '}
-                                </Text>
-                                : null}
-                            {numberOfLines > 2
-                                ? <Text style={styles.eventTimes} numberOfLines={1}>{moment(event.start).format(formatTime)} - {moment(event.end).format(formatTime)}</Text>
-                                : null}
-                        </TouchableOpacity>
-                    )}
-                </View>
-            )
-        })
+  _renderLines() {
+    const { format24h, start, end } = this.props;
+    const offset = this.calendarHeight / (end - start);
 
-        return (
-            <View>
-                <View style={{ marginLeft: LEFT_MARGIN }}>
-                    {events}
-                </View>
-            </View>
-        )
-    }
+    return range(start, end + 1).map((i, index) => {
+      let timeText;
+      if (i === start) {
+        timeText = ``;
+      } else if (i < 12) {
+        timeText = !format24h ? `${i} AM` : i;
+      } else if (i === 12) {
+        timeText = !format24h ? `${i} PM` : i;
+      } else if (i === 24) {
+        timeText = !format24h ? `12 AM` : 0;
+      } else {
+        timeText = !format24h ? `${i - 12} PM` : i;
+      }
+      const { width, styles } = this.props;
+      return [
+        <Text
+          key={`timeLabel${i}`}
+          style={[styles.timeLabel, { top: offset * index - 6 }]}
+        >
+          {timeText}
+        </Text>,
+        i === start ? null : (
+          <View
+            key={`line${i}`}
+            style={[styles.line, { top: offset * index, width: width - 20 }]}
+          />
+        ),
+        <View
+          key={`lineHalf${i}`}
+          style={[
+            styles.line,
+            { top: offset * (index + 0.5), width: width - 20 },
+          ]}
+        />,
+      ];
+    });
+  }
 
-    render () {
-        const { styles } = this.props
-        return (
-            <ScrollView ref={ref => (this._scrollView = ref)}
-                        contentContainerStyle={[styles.contentStyle, { width: this.props.width }]}
+  _renderTimeLabels() {
+    const { styles, start, end } = this.props;
+    const offset = this.calendarHeight / (end - start);
+    return range(start, end).map((item, i) => {
+      return (
+        <View key={`line${i}`} style={[styles.line, { top: offset * i }]} />
+      );
+    });
+  }
+
+  _onEventTapped(event) {
+    this.props.eventTapped(event);
+  }
+
+  _renderEvents() {
+    const { styles } = this.props;
+    const { packedEvents } = this.state;
+    let events = packedEvents.map((event, i) => {
+      const style = {
+        left: event.left,
+        height: event.height,
+        width: event.width,
+        top: event.top,
+      };
+
+      const eventColor = {
+        backgroundColor: event.color,
+      };
+
+      // Fixing the number of lines for the event title makes this calculation easier.
+      // However it would make sense to overflow the title to a new line if needed
+      const numberOfLines = Math.floor(event.height / TEXT_LINE_HEIGHT);
+      const formatTime = this.props.format24h ? 'HH:mm' : 'hh:mm A';
+      return (
+        <View key={i} style={[styles.event, style, event.color && eventColor]}>
+          {this.props.renderEvent ? (
+            this.props.renderEvent(event)
+          ) : (
+            <TouchableOpacity
+              activeOpacity={0.5}
+              onPress={() =>
+                this._onEventTapped(this.props.events[event.index])
+              }
             >
-                {this._renderLines()}
-                {this._renderEvents()}
-                {this._renderRedLine()}
-            </ScrollView>
-        )
-    }
+              <Text numberOfLines={1} style={styles.eventTitle}>
+                {event.title || 'Event'}
+              </Text>
+              {numberOfLines > 1 ? (
+                <Text
+                  numberOfLines={numberOfLines - 1}
+                  style={[styles.eventSummary]}
+                >
+                  {event.summary || ' '}
+                </Text>
+              ) : null}
+              {numberOfLines > 2 ? (
+                <Text style={styles.eventTimes} numberOfLines={1}>
+                  {moment(event.start).format(formatTime)} -{' '}
+                  {moment(event.end).format(formatTime)}
+                </Text>
+              ) : null}
+            </TouchableOpacity>
+          )}
+        </View>
+      );
+    });
+
+    return (
+      <View>
+        <View style={{ marginLeft: LEFT_MARGIN }}>{events}</View>
+      </View>
+    );
+  }
+
+  render() {
+    const { styles } = this.props;
+    return (
+      <ScrollView
+        ref={ref => (this._scrollView = ref)}
+        contentContainerStyle={[
+          styles.contentStyle,
+          { width: this.props.width },
+        ]}
+      >
+        {this._renderLines()}
+        {this._renderEvents()}
+        {this._renderRedLine()}
+      </ScrollView>
+    );
+  }
 }
