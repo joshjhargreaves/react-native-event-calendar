c79a593458811f2f561e4764ce817f7a5138f55d
Johan <21sebas12@gmail.com>
2020-08-05 14:20:49 -0500

    set property half

--- src/DayView.js
+++ src/DayView.js
@@ -136,7 +136,7 @@ export default class DayView extends React.PureComponent {
             { top: offset * (index + 0.5), width: width - 20,  marginTop:4   },
           ]}
         />,
-        onPressHour && (<TouchableNativeFeedback  onPress={() => onPressHour({...value, time:this._formatHour({format24h, hour:i, start, half:':30'})})}     key={`touch${i}`}>
+        onPressHour && (<TouchableNativeFeedback  onPress={() => onPressHour({...value, half:':30',time:this._formatHour({format24h, hour:i, start, half:':30'})})}     key={`touch${i}`}>
           <View
             key={`lineHalf${i}`}
             style={[
